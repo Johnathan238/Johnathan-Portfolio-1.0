208398bdb89bb844a0270b65f7905fdc5d31e8a3
Johnathan Umana <jonathanumana86@gmail.com.com>
2020-09-16 14:17:02 -0400

    Its working

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ navLinks.forEach((link) => {
 
 const options = {
   bottom: "64px", // default: '32px'
-  right: "unset", // default: '32px'
-  left: "32px", // default: 'unset'
+  right: "32px", // default: '32px'
+  left: "unset", // default: 'unset'
   time: "0.5s", // default: '0.3s'
   mixColor: "#fff", // default: '#fff'
   backgroundColor: "#fff", // default: '#fff'
-  buttonColorDark: "#100f2c", // default: '#100f2c'
+  buttonColorDark: "#000", // default: '#100f2c'
   buttonColorLight: "#fff", // default: '#fff'
   saveInCookies: false, // default: true,
   label: "🌓", // default: ''
