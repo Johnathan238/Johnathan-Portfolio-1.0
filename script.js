740704ba0c875522b3b2c1991cc2335913c1d10a
Johnathan238 <jonathanumana86@gmail.com>
2021-04-14 01:31:30 -0400

    Will refresh everything

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const options = {
   time: "0.5s", // default: '0.3s'
   mixColor: "#fff", // default: '#fff'
   backgroundColor: "#fff", // default: '#fff'
-  buttonColorDark: "#000", // default: '#100f2c'
+  buttonColorDark: "100f2c", // default: '#100f2c'
   buttonColorLight: "#fff", // default: '#fff'
   saveInCookies: false, // default: true,
   label: "🌓", // default: ''
